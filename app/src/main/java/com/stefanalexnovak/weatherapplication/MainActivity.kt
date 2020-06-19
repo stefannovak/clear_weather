7b4105ac2a781937daf701fda7846817d088127a
Stefan Novak <stefannovak96@gmail.com>
2020-06-19 11:21:15 +0100

    Allowed the app access to the internet, and set it to permanently portrait mode

--- app/src/main/java/com/stefanalexnovak/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/stefanalexnovak/weatherapplication/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val APIkey = "ee3cc93e43ef00b96a6bb4e56902d020"
     }
 }
